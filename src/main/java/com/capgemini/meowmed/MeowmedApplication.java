aecd93225c4036146677ea44e68860fac509efc6
Mithra <m.rabet@ostfalia.de>
2023-10-18 20:01:57 +0200

    Initial commit

---
+++ src/main/java/com/capgemini/meowmed/MeowmedApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.meowmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeowmedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeowmedApplication.class, args);
+    }
+
+}
