9205dd893ce59c6f08e39c75cefe56cc1871dd9b
Mithra <m.rabet@ostfalia.de>
2023-10-21 09:29:20 +0200

    add Rest API at update & delete

--- src/main/java/com/capgemini/meowmed/controller/CustomerController.java
+++ src/main/java/com/capgemini/meowmed/controller/CustomerController.java
@@ -4,13 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.capgemini.meowmed.exception.ResourceNotFoundException;
 import com.capgemini.meowmed.model.Customer;
@@ -53,6 +47,7 @@ public class CustomerController {
     }
 
     //Update customer
+    @PutMapping("/kunden/{id}")
     public ResponseEntity<Customer> updateCustomer(@PathVariable(value = "id") Long customerId, @Valid @RequestBody Customer customerDetails) throws ResourceNotFoundException{
 
         Customer customer = customerRepository.findById(customerId)
@@ -67,14 +62,16 @@ public class CustomerController {
     }
 
     //Delete customer
-    public Map<String, Boolean> deleteCustomer(@PathVariable(value = "id") Long customerId) throws ResourceNotFoundException{
+    @DeleteMapping("/kunden/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteCustomer(@PathVariable(value = "id") Long customerId) throws ResourceNotFoundException{
         Customer customer = customerRepository.findById(customerId)
         .orElseThrow(() -> new ResourceNotFoundException("Es gibt keinen Kunden mit dieser id: " + customerId));
 
         customerRepository.delete(customer);
+        //Weil die delete Methode nichts zurückgibt, wird noch eine Map benötigt, die zurückgegeben wird
         Map<String, Boolean> response = new HashMap<>();
-        response.put("gelöscht", Boolean.TRUE);
-        return response;
+        response.put("Der Kunde wurde gelöscht", Boolean.TRUE);
+        return ResponseEntity.ok(response);
     }
 
 
